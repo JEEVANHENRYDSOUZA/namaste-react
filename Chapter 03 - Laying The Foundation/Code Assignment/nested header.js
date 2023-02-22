29c919b8eed40edf5daed36473cb0c2b9d98ea30
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2023-02-21 22:45:45 -0500

    Create nested header.js

---
+++ Chapter 03 - Laying The Foundation/Code Assignment/nested header.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="title">
+      <h1>nested</h1>
+      <h2>element</h2>
+      <h3>test</h3>
+    </div>
+  );
+}
+
+export default App;
